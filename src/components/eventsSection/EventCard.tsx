9c6c5e79884add86a026913e51abe2363728678d
KozerT <kozer.vizastaff@gmail.com>
2024-03-05 21:44:29 +0100

    feat: implement functionality for Entrance Fee data output

--- src/components/eventsSection/EventCard.tsx
+++ src/components/eventsSection/EventCard.tsx
@@ -9,7 +9,6 @@ interface EventCardProps {
 
 export default function EventCard({ event }: EventCardProps) {
   const { formattedDate, formattedTime } = formatDateTime(event.dateAndTime)
-
   return (
     <li className="flex flex-col overflow-hidden border shadow-md cursor-pointer group rounded-xl font-roboto">
       <div className="relative w-full overflow-hidden min-h-[183px] transition-transform duration-200 ease-out border rounded-xl group-hover:scale-105">
@@ -22,17 +21,22 @@ export default function EventCard({ event }: EventCardProps) {
       <div className="flex-1 mx-5 my-3 ">
         <p className="text-xl font-medium text-[#21272A]"> {event.title}</p>
         <p className="mt-3 text-base  text-[#525252] ">
-          {formattedDate}
-          <span className="mx-2">•</span>
-          <span>{formattedTime}</span>
+        {formattedDate && formattedTime && (
+            <>
+              {formattedDate} <span className="mx-2">•</span>
+              <span>{formattedTime}</span>
+            </>
+          )}
         </p>
         <address className="text-base not-italic text-black line-clamp-2 s ">
           {event.location?.address}, {event.location?.city}
         </address>
       </div>
-      <p className="flex items-center mx-5 mb-5 font-medium text-[#057910] ">
-        Entrance Fee
-      </p>
+      {event.entranceFee && (
+        <p className={`flex items-center mx-5 mb-5 font-medium text-${event.entranceFee.type === 'Free' ? '[#057910]' : '[#0F62FE]'}`}>
+          {event.entranceFee.type === 'Free' ? 'Free' : `${event.entranceFee.priceSek} SEK`}
+        </p>
+      )}
     </li>
   )
 }
