7a4d1c7823922b196b477de39497134c1d2a439a
KozerT <kozer.vizastaff@gmail.com>
2024-03-05 23:22:08 +0100

    feat: enable external link to redirect to Google Maps location

--- src/components/eventsSection/EventCard.tsx
+++ src/components/eventsSection/EventCard.tsx
@@ -9,6 +9,25 @@ interface EventCardProps {
 
 export default function EventCard({ event }: EventCardProps) {
   const { formattedDate, formattedTime } = formatDateTime(event.dateAndTime)
+
+  const renderLocationLink = () => {
+    const { address, city, googleMapsUrl } = event.location ?? {}
+    return (
+      address &&
+      city &&
+      googleMapsUrl && (
+        <a
+          href={googleMapsUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-base not-italic text-black  hover:text-[#0F62FE] hover:underline line-clamp-2"
+        >
+          {`${address}, ${city}`}
+        </a>
+      )
+    )
+  }
+
   return (
     <li className="flex flex-col overflow-hidden border shadow-md cursor-pointer group rounded-xl font-roboto">
       <div className="relative w-full overflow-hidden min-h-[183px] transition-transform duration-200 ease-out border rounded-xl group-hover:scale-105">
@@ -21,20 +40,22 @@ export default function EventCard({ event }: EventCardProps) {
       <div className="flex-1 mx-5 my-3 ">
         <p className="text-xl font-medium text-[#21272A]"> {event.title}</p>
         <p className="mt-3 text-base  text-[#525252] ">
-        {formattedDate && formattedTime && (
+          {formattedDate && formattedTime && (
             <>
               {formattedDate} <span className="mx-2">•</span>
               <span>{formattedTime}</span>
             </>
           )}
         </p>
-        <address className="text-base not-italic text-black line-clamp-2 s ">
-          {event.location?.address}, {event.location?.city}
-        </address>
+        {renderLocationLink()}
       </div>
       {event.entranceFee && (
-        <p className={`flex items-center mx-5 mb-5 font-medium text-${event.entranceFee.type === 'Free' ? '[#057910]' : '[#0F62FE]'}`}>
-          {event.entranceFee.type === 'Free' ? 'Free' : `${event.entranceFee.priceSek} SEK`}
+        <p
+          className={`flex items-center mx-5 mb-5 font-medium ${event.entranceFee.type === 'Free' ? 'text-[#057910]' : 'text-[#0F62FE]'}`}
+        >
+          {event.entranceFee.type === 'Free'
+            ? 'Free'
+            : `${event.entranceFee.priceSek} SEK`}
         </p>
       )}
     </li>
