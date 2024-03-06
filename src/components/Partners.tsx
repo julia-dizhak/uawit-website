bd8631b3a50410eef9eaf4608f78a4c7a601b463
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-03-06 10:41:18 +0100

    added design

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,6 +1,7 @@
 import { Partner } from '~/lib/sanity.queries/partners/types'
 import Image from 'next/image'
 import { urlForImage } from '~/lib/sanity.image'
+import Container from './Container'
 
 type Props = {
   partners: Partner[]
@@ -8,21 +9,21 @@ type Props = {
 
 export const Partners = ({ partners }: Props) => {
   return (
-    <div className="text-center">
+    <Container className="text-center">
       <h2 className="text-2xl font-bold my-4">Our Partners</h2>
-      <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap justify-center text-center align-center">
         {partners.map((partner: Partner, index) => (
-          <a href={partner.href} key={index} className="block w-1/4 p-4">
+          <a href={partner.href} key={index} className="block m-auto w-1/4 p-4">
             <Image
-              className="w-full h-auto"
+              className="block text-center m-auto"
               src={urlForImage(partner.partnerLogo).url()}
-              height={50}
-              width={50}
+              height={60}
+              width={60}
               alt={partner.partnerName}
             />
           </a>
         ))}
       </div>
-    </div>
+    </Container>
   )
 }
