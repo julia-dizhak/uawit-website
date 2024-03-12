5cd068de24649b98db79e3edc64465fe5059818b
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-03-12 13:43:00 +0100

    Fix conflicts in main branch

---
+++ src/components/ContentSection.tsx
@@ -0,0 +1,32 @@
+import EventCard from './EventCard'
+import { EventType, EventsListType } from '~/lib/sanity.queries/events/types'
+import Container from './Container'
+
+type Props = {
+  events: EventsListType
+}
+export default function ContentSection({ events }: Props) {
+  // TODO: sort - > cards by date;
+
+  return (
+    <Container className="text-center">
+      <h2 className="font-bold text-center text-[42px]  text-[#21272A] font-roboto ">
+        Event Title
+      </h2>
+
+      <p className="text-center font-roboto color-[#21272A] mt-4 font-normal text-lg  mb-[3.75rem]">
+        Paragraph. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Praesentium doloribus et nulla saepe ullam quibusdam cum laudantium
+        voluptatem.
+      </p>
+
+      <ul className="container grid grid-cols-1 gap-[3.75rem]  md:grid-cols-2 lg:grid-cols-3  ">
+        {events.map((event: EventType) => (
+          <EventCard event={event} key={event.title} />
+        ))}
+      </ul>
+
+      <button className="mt-[3.75rem]">See more Events </button>
+    </Container>
+  )
+}
