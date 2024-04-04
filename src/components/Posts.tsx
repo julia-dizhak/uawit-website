2ca70f2fcc54e50a7ca4da36e58d4c135cf98aa1
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-04-04 17:21:49 +0200

    support responsive

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const Posts = ({ posts }: Props) => {
   return (
     <div className="bg-backgroundColorGray relative">
-      <div className="absolute top-0 left-0 z-1">
+      <div className="absolute top-0 left-0 z-1 lg:visible">
         <div className="w-[200]px h-[150px]">
           <Image
             src={decorativeImage}
