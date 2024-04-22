0b261a5f052d93110700575445a6a6a5e88eb535
tetianamamchyk <tetiana.mamchyk@impactcommerce.se>
2024-04-22 08:10:14 +0200

    Change container component path

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { urlForImage } from '~/lib/sanity.image'
 import { WidgetType } from '~/lib/sanity.queries/widgets/types'
-import Container from './Container'
+import Container from './common/Container'
 import { Logo } from './Logo'
 import React from 'react'
 
@@ -14,7 +14,6 @@ export const Widget = ({ widget }: Props) => {
     title,
     description,
     componentType,
-    Button,
     logo,
     Picture
   } = widget;
@@ -22,6 +21,7 @@ export const Widget = ({ widget }: Props) => {
   console.log(widget);
 
   if (widget.componentType === 'Image') {
+    // @ts-ignore
     return (
       <Container className="text-center">
         <div>Widget should be here</div>
@@ -40,6 +40,7 @@ export const Widget = ({ widget }: Props) => {
       </Container>
     )
   } else if (widget.componentType === 'Logo') {
+    // @ts-ignore
     return (
       <Logo logo={widget.logo} />
     )
