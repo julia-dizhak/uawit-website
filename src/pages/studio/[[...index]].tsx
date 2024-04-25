15dd6732a6d586a26a81f4467eaa399a57ad62f6
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-04-25 23:11:09 +0200

    move it to app

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,19 +1,15 @@
 import Head from 'next/head'
 import { NextStudio } from 'next-sanity/studio'
-import { metadata } from 'next-sanity/studio/metadata'
+import { metadata as studioMetadata } from 'next-sanity/studio'
 import config from 'sanity.config'
 
 export default function StudioPage() {
   return (
     <>
       <Head>
-        {Object.entries(metadata).map(([key, value]) => (
+        {Object.entries(studioMetadata).map(([key, value]) => (
           <>
             <meta key={key} name={key} content={value} />
-            <link
-              rel="shortcut icon"
-              href="/src/favicon.ico"
-            />
           </>
         ))}
       </Head>
